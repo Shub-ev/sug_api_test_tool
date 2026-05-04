3df75fff4142ed9f754090f2c2ccf489d0d50885
shubham <shubham@shub-kali.shub>
2026-05-04 21:09:25 +0530

    Create Sidebar for APP

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,13 @@
+const Sidebar = () => {
+
+    return (
+        <div className={"sidebar border-1 border-white"}>
+            {/* Create Collection Row */}
+            <div className={"flex flex-row"}>
+
+            </div>
+        </div>
+    )
+};
+
+export default Sidebar;
